c17e8bdf9896f241bffb83e254197331fd722a0a
Han5olo <m.accounts@mailbox.org>
2025-11-16 17:11:22 +0100

    added plottig functio

--- process_audio.py
+++ process_audio.py
@@ -1,15 +1,21 @@
 # %%
 # Parameters
-clip_duration = 10   # seconds
-clip_delay = 25       # seconds after CSV time before clip starts
+import vars
 
 custom_audio_path = None
-custom_csv_path = '/home/dorian/Desktop/OsloCSV/rec'
-custom_audio_path = '/home/dorian/DTUMaster/data/raw/2025-10_OsloWaveTank/AudioMoth'
+custom_csv_path = None
+v = vars.get_vars() 
+
+# Defining variabels
+custom_csv_path = v['custom_csv_path']
+custom_audio_path = v['custom_audio_path']
+clip_duration = v['clip_duration']
+clip_delay = v['clip_delay']
 
 import numpy as np
 import pandas as pd
 import scipy.io.wavfile as wav
+from scipy import signal
 import os
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -29,7 +35,7 @@ csv_files = sorted([f for f in csv_path.glob('*.csv')])
 for csv_file in csv_files:
     logger.info(f"Found CSV file: {csv_file}")
 
-    #%% Read CSV file as pandas DataFrame
+    # Read CSV file as pandas DataFrame
     df = pd.read_csv(csv_file)
 
     #ids = df['ID']
@@ -40,7 +46,7 @@ for csv_file in csv_files:
     # Convert data and time to datetime object with format YYYY-MM-DD HH:MM:SS
     date_time_UTC = pd.to_datetime(date + ' ' + time_UTC, format='%Y-%m-%d %H:%M:%S')
 
-    #%% Locate audio files
+    # Locate audio files
     audio_path = Path(custom_audio_path or Path.cwd() / 'input')
     audio_files = sorted([f for f in audio_path.rglob('*.WAV')])
     logger.info(f"Found {len(audio_files)} audio files in {audio_path}")
@@ -51,7 +57,7 @@ for csv_file in csv_files:
         # Load Stat time from filename
         start_time = func.get_start_time_from_filename(audio_file)
 
-        #%% Read audio file duration using wave module
+        # Read audio file duration using wave module
         logger.info("Reading audio file duration...")
         with wave.open(str(audio_file), 'rb') as wf:
             frames = wf.getnframes()
@@ -59,16 +65,14 @@ for csv_file in csv_files:
             duration_seconds = frames / float(rate)
             duration = int(duration_seconds * 1e9) # To nanoseconds
 
-        #%%
-
         end_time = func.calc_end_time(start_time, duration)
 
-        #%% Find recorded timestamps within audio file time range
+        # Find recorded timestamps within audio file time range
 
         # Create boolean mask for timestamps within audio file time range
         mask = func.get_time_range_mask(date_time_UTC, start_time, end_time)
 
-        #%% Create audio clips for recorded timestamps within audio file time range
+        # Create audio clips for recorded timestamps within audio file time range
         if any(mask):
             logger.info("Recorded timestamps found within audio file time range.")
 
@@ -94,7 +98,7 @@ for csv_file in csv_files:
                                            clip_duration)
 
             logger.info("Finished creating audio clips.")
-            del data, rec_timestamps, ids, runs  # Free up memory
+            #del data, rec_timestamps, ids, runs  # Free up memory
             logger.info("------------------------------------------------------")
         else:
             logger.info("No recorded timestamps within audio file time range.")
@@ -102,4 +106,18 @@ for csv_file in csv_files:
 
 logger.info("All done!")
 logger.info("******************************************************")
-# %%
+
+
+def plot_downsample_data(ydem, duration):
+    xdem = np.linspace(0, duration, len(ydem))
+    plt.plot(xdem, ydem)
+    plt.legend(['data'], loc='best')
+    plt.show()
+
+
+q =1000
+
+samples_decimated = int(len(data)/q)
+ydem = signal.decimate(data, q)
+
+plot_downsample_data(ydem, samplerate, duration)
