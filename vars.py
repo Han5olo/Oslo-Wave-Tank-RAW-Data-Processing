0e15a4bd8701693cb30ed2e420a9be000499ce2f
Han5olo <m.accounts@mailbox.org>
2025-11-16 17:11:59 +0100

    added function file to create and load cutom variabels

---
+++ vars.py
@@ -0,0 +1,11 @@
+def get_vars():
+
+    var_dict = {
+        'custom_csv_path':'/home/dorian/Desktop/OsloCSV/rec',
+        'custom_audio_path':'/home/dorian/DTUMaster/data/raw/2025-10_OsloWaveTank/AudioMoth',
+        'clip_duration': 10, # seconds
+        'clip_delay': 25 # seconds 
+    }
+
+
+    return var_dict
